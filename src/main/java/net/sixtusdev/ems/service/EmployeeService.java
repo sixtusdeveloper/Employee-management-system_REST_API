379fa20b8dfb754aa7d8e069aeae03bc201fb543
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-16 13:31:33 +0100

    implemented saveEmployee logic

---
+++ src/main/java/net/sixtusdev/ems/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package net.sixtusdev.ems.service;
+
+import net.sixtusdev.ems.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+}
---
+++ src/main/java/net/sixtusdev/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package net.sixtusdev.ems.service.impl;
+
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+import net.sixtusdev.ems.dto.EmployeeDto;
+import net.sixtusdev.ems.entity.Employee;
+import net.sixtusdev.ems.mapper.EmployeeMapper;
+import net.sixtusdev.ems.repository.EmployeeRepository;
+import net.sixtusdev.ems.service.EmployeeService;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee saveEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(saveEmployee);
+    }
+
+}
