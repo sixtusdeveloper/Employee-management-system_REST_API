6728282d8ee824c3cb2b1eeb5f8be2c57c8e30a9
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-17 16:15:09 +0100

    resolved repo name

--- src/main/java/net/sixtusdev/ems/repository/EmployeeRepository.java
+++ src/main/java/net/sixtusdev/ems/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.sixtusdev.ems.entity.Employee;
 
+// EmployeeRepository.java
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
